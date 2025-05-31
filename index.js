9dba424b803d765533789472eb3506b843f0d7b0
shakib <hossenmdshakib47@gmail.com>
2025-06-01 00:10:11 +0600

    initialized the server setup and mongodb database connected

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express()
+require('dotenv').config()
+
+const port=process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_ROMHATCH_DB}:${process.env.PASS_ROMHATCH_DB}@cluster0.ab8v5sj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/",(req,res)=>{
+    res.send("roomHatch server has find new roomate");
+});
+app.listen(port,()=>{
+    console.log(`roomHatch server is running on port ${port}`);
+})
