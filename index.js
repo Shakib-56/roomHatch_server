747ee6307eea266c069be0a81c52715c4de085fc
shakib <hossenmdshakib47@gmail.com>
2025-06-06 19:15:49 +0600

    get input data from database

--- index.js
+++ index.js
@@ -45,6 +45,20 @@ async function run() {
     const users=await userCollection.find().toArray()
     res.send(users);
   })
+  app.get("/users/:email",async(req,res)=>{
+      try {
+    const email = req.params.email;
+    const user = await userCollection.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching user", error });
+  }
+  })
 
 // read
 app.get("/roommates", async (req, res) => {
